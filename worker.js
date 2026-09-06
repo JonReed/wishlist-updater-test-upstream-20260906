3977d828282368a667fb2c427dbcd2a484bcba00
JonReed <jon@mail24.co.uk>
2026-09-06 10:16:47 +0100

    Synthetic version stable-7 for failed build and retry check

--- worker.js
+++ worker.js
@@ -1,1 +1,1 @@
-export default { fetch(request, env) { return Response.json({ version: "stable-6", installation: env.INSTALLATION_MARKER }); } };
+export default { fetch(request, env) { return Response.json({ version: "stable-7-retry-check", installation: env.INSTALLATION_MARKER }); } };
